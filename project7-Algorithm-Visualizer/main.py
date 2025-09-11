a656257becf32de66ed175518b8e136fcd30562f
Zach <PatzerZach@outlook.com>
2025-09-10 21:29:05 -0500

    Implemented bubble sort, not with visuals, but it is working

--- project7-Algorithm-Visualizer/main.py
+++ project7-Algorithm-Visualizer/main.py
@@ -4,20 +4,19 @@ from tkinter import *
 bubble_pressed = False
 
 def bubble_sort(array):
-    n = len(array)
-    
-    for i in range(n):
-        swapped = FALSE
+    if bubble_pressed == True:
+        n = len(array)
         
-        for j in range(0, n-i-1):
-            if array[j] > array[j+1]:
-                array[j], array[j+1] = array[j+1], array[j]
-                swapped = True
-        if (swapped == False):
-            break
-
-def visualize(num_list):
-    return
+        for i in range(n):
+            swapped = False
+            
+            for j in range(0, n-i-1):
+                if array[j] > array[j+1]:
+                    array[j], array[j+1] = array[j+1], array[j]
+                    swapped = True
+            if (swapped == False):
+                break
+        print(array)
 
 def bubble_button():
     global bubble_pressed
@@ -26,22 +25,22 @@ def bubble_button():
 def start():
     num_inputs = num_input.get().replace(" ", "").split(",")
     num_list = [int(num) for num in num_inputs]
-    visualize(num_list)
+    bubble_sort(num_list)
 
 def main():
     global num_input
     root = tk.Tk()
     root.title("Algorithm Visualizer")
-    root.geometry("1000x700")
-    sort_label = Label(root, text="Select a Sort", font=("Helvetica", 16))
+    root.geometry("1400x900")
+    sort_label = Label(root, text="Select a Sort", font=("Helvetica", 14))
     sort_label.pack()
-    bubble_sort = Button(root, text="Bubble Sort", command=bubble_button, font=("Helvetica", 16))
+    bubble_sort = Button(root, text="Bubble Sort", command=bubble_button, font=("Helvetica", 14))
     bubble_sort.pack()
-    numbers_label = Label(root, text="Please enter a comma seperated list of numbers", font=("Helvetica", 16))
+    numbers_label = Label(root, text="Please enter a comma seperated list of numbers", font=("Helvetica", 14))
     numbers_label.pack()
     num_input = Entry(root, width=50, font=("Helvetica", 16))
     num_input.pack()
-    start_button = Button(root, text="Start", command=start, font=("Hevletica", 18))
+    start_button = Button(root, text="Start", command=start, font=("Hevletica", 14))
     start_button.pack()
     
     
